2a30737be3f245c4a831549a8299a462453173c4
stf <s@ctrlc.hu>
2015-03-01 03:56:14 +0100

    [mod] sig/crypto.c simplified verify loop. still not working for msglen>48088

--- sig/crypto.c
+++ sig/crypto.c
@@ -13,7 +13,7 @@
 #include "sphincs256.h"
 #include "blake512.h"
 
-#define BUFSIZE 65536
+#define BUFSIZE (1<<16)
 
 FILE* keyopen(char* prefix, char* postfix) {
   size_t fnlen = strlen(prefix)+strlen(postfix)+1;
@@ -83,11 +83,11 @@ int sig_sign(void* sk) {
   }
 
   // calculate sig and output
-  unsigned long long hash[8];
-  u8 sm[CRYPTO_BYTES+sizeof(hash)];
+  u8 hash[BLAKE512_BYTES];
+  u8 sm[CRYPTO_BYTES+BLAKE512_BYTES];
   unsigned long long smlen;
-  blake512_final( &S, (u8*) &hash );
-  if(crypto_sign(sm, &smlen,(u8*) &hash, sizeof(hash), sk) == -1) {
+  blake512_final( &S, hash );
+  if(crypto_sign(sm, &smlen, hash, BLAKE512_BYTES, sk) == -1) {
     fprintf(stderr, "signing failed\n");
     return 1;
   }
@@ -100,8 +100,7 @@ int sig_sign(void* sk) {
 }
 
 int sig_verify(void* pk) {
-  int ret;
-  unsigned char buf[BUFSIZE];
+  unsigned char buf[BUFSIZE], *hash = buf + CRYPTO_BYTES;
   size_t size;
   blake512_state S;
   blake512_init( &S );
@@ -109,25 +108,28 @@ int sig_verify(void* pk) {
   // hash incoming stdin to stdout while always retaining the last
   // CRYPTO_BYTES to be able to use them to verify the message tag
   size=fread(buf, 1, BUFSIZE, stdin);
-  while(size > CRYPTO_BYTES) {
-    if(!_write(buf, size-CRYPTO_BYTES)) {
+  if(size < CRYPTO_BYTES) {
+    fprintf(stderr, "\ntruncated signature\n");
+    return 1;
+  }
+  size-=CRYPTO_BYTES;
+  while(size > 0) {
+    if(!_write(buf, size)) {
       return 1;
     }
-    blake512_update( &S, buf, (size-CRYPTO_BYTES)*8 );
-    // move last unhashed bytes to  to the beginning of buf
-    memcpy(buf, buf+(size-CRYPTO_BYTES), CRYPTO_BYTES);
-    if((ret = fread(buf+CRYPTO_BYTES, 1, BUFSIZE-CRYPTO_BYTES, stdin))>0) {
-      size=CRYPTO_BYTES+ret;
-    } else {
-      size=CRYPTO_BYTES;
-    }
+    blake512_update( &S, buf, size*8 );
+    // move last unhashed bytes to the beginning of buf
+    memmove(buf, buf+size, CRYPTO_BYTES);
+    size = fread(buf+CRYPTO_BYTES, 1, BUFSIZE-CRYPTO_BYTES, stdin);
   }
   fflush(stdout);
 
-  blake512_final( &S, (u8*) buf+CRYPTO_BYTES );
-  unsigned long long msg[8];
+  blake512_final( &S, hash );
+
+  u8 msg[BLAKE512_BYTES];
   unsigned long long msglen;
-  if(crypto_sign_open((u8*) &msg, &msglen, buf, CRYPTO_BYTES+sizeof(msg), pk) == -1) {
+
+  if(crypto_sign_open(msg, &msglen, buf, CRYPTO_BYTES+BLAKE512_BYTES, pk) == -1) {
     fprintf(stderr, "\nverification failed\n");
     return 1;
   }
